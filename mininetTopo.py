b43b48473a4bff76eee995bad951829355791d90
GeNiaaz <geniaaz1009@gmail.com>
2021-11-17 22:01:43 +0800

    add ip

--- mininetTopo.py
+++ mininetTopo.py
@@ -100,7 +100,7 @@ def startNetwork():
 
     global net
     net = Mininet(topo=topo, link = Link,
-                  controller=lambda name: RemoteController(name, ip='SERVER IP'),
+                  controller=lambda name: RemoteController(name, ip='127.0.0.1'),
                   listenPort=6633, autoSetMacs=True)
 
     info('** Starting the network\n')
